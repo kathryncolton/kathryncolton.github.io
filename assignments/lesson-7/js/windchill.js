59e32439cd7912f59765f450bcdd5ae9b7042324
kathryncolton <kathryncolton@yahoo.com>
2018-02-23 21:34:58 -0800

    edits

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -7,6 +7,6 @@ var avTemp = (h + l) / 2;
 
 var f = 35.74 + 0.6215 * avTemp - 35.75 * Math.pow(s, 0.16) + 0.4275 * avTemp * Math.pow(s, 0.16);
 
-f = Math.round(f * 100) / 100.0;
+f = Math.round(f * 10) / 10.0;
 
 document.getElementById("windchill").innerHTML = f;
