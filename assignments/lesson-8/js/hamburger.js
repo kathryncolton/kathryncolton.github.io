d1ad3aaf84fa7f2637a8b086acfb22e5bc8cf616
kathryncolton <kathryncolton@yahoo.com>
2018-03-02 21:58:27 -0800

    added storm center page

---
+++ assignments/lesson-8/js/hamburger.js
@@ -0,0 +1,5 @@
+/* Toggle between adding and removing the "hide" class to topnav when the user clicks on the icon */
+
+function hamburger() {
+document.getElementById("navigation").classList.toggle("hide");
+}
